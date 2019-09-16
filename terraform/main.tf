eb5b90f6260620783db7eb0e81249e3b685e7501
Tony Le <heytonyle@icloud.com>
2019-09-15 17:12:49 -0700

    Add S3 config and public read policy

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,4 +1,7 @@
-// We're using AWS as the cloud provider.
+// Architecture defaults listed at:
+// https://github.com/spiritphyz/AWS-Serverless-Guides
+
+// Use AWS as the cloud provider.
 provider "aws" {
   region = "us-west-1"
 }
@@ -11,4 +14,29 @@ variable "www_domain_name" {
 // Create a variable for the apex domain (also called root or "naked" domain).
 variable "apex_domain_name" {
   default = "spiritphyz.io"
+}
+
+resource "aws_s3_bucket" "www" {
+	bucket = "${var.www_domain_name}"
+	acl    = "public-read"
+	policy = <<POLICY
+	{
+		"Version":"2012-10-17",
+		"Statement":[
+			{
+				"Sid":"AddPerm",
+				"Effect":"Allow",
+				"Principal": "*",
+				"Action":["s3:GetObject"],
+				"Resource":["arn:aws:s3:::${var.www_domain_name}/*"]
+			}
+		]
+	}
+	POLICY
+
+	website {
+		index_document = "index.html"
+		error_document = "error.html"
+		//error_document = "index.html"
+	}
 }
